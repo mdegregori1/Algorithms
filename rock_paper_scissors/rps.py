f04305bf599b14efc338d58247381c2a78519fb3
Mauricio Degregori <degregori2@gmail.com>
2020-04-15 15:43:03 -0500

    emptied proj files + added fib.py notes from lecture

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,22 +3,7 @@
 import sys
 
 def rock_paper_scissors(n):
-  #set an empty array to add all plays
-  total_plays = []
-  def rps_plays(total_plays, plays, n):
-    rps = ['rock', 'paper', 'scissors']
-    # if the length of arrays equal the input, then add plays to total plays array
-    if len(plays) == n:
-      total_plays.append(plays)
-    #if not, loops through rps array and add new element to new plays 
-    else:
-      for g in rps:
-        new_plays = plays.copy()
-        new_plays.append(g)
-        rps_plays(total_plays, new_plays, n)
-
-  rps_plays(total_plays, [], n)
-  return total_plays
+  pass 
 
 
 if __name__ == "__main__":
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,22 +3,7 @@
 import argparse
 
 def find_max_profit(prices):
-  #set a minimum price at index zero 
-  #set a current profit function where you have [1] - [0]
-  #^ this is because profit is max - min, and max has to be after min 0 
-  current_min_price = prices[0]
-  current_profit = prices[1] - prices[0]
-  # now, we have to loop through the array to get all of the elements 
-  # start at 1, as 0 is where your min price is
-  for x in range(0, len(prices)):
-    # if current minimum price is greater than prices[x], that means that prices x is lower. So, it should now #be the lower price
-    if current_min_price > prices[x]:
-      current_min_price = prices[x]
-    # if the current profit is less than prices[x] minus minimum profit, then profit is not maximized. So, it #should now be set to current_profit
-    elif current_profit < prices[x] - current_min_price:
-      current_profit = prices[x] - current_min_price
-
-  return current_profit 
+  pass
 
 
 if __name__ == '__main__':
